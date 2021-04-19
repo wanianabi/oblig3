69535d4504cd6d7bcb5b87e7dffe1af6a87bfc25
wanianabi <73027290+wanianabi@users.noreply.github.com>
2021-04-19 22:11:57 +0200

    schema.sql oppdatert

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE BILLETTER
     adresse VARCHAR(255) NOT NULL,
     telefonnr VARCHAR(255) NOT NULL,
     epost VARCHAR(255) NOT NULL,
-    antall VARCHAR(255) NOT NULL
+    antall VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
